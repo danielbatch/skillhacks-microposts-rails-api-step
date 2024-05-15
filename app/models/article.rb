e4a846bc23bb113bb2504bc04679e8eab64f841d
danielbatch <104686031+danielbatch@users.noreply.github.com>
2024-05-15 09:27:38 +0000

    バリデーションについて学ぼうまで

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ApplicationRecord
+    TITLE_MAX_LENGTH = 20
+    BODY_MAX_LENGTH = 150
+
     belongs_to :category
+    enum status: { draft: "draft", published: "published" }
+    validates :title, presence: true, length: { maximum: TITLE_MAX_LENGTH }
+    validates :body, presence: true, length: { maximum: BODY_MAX_LENGTH }
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :articles
+    validates :name, presence: true, uniqueness: { case_sensitive: true }
 end
