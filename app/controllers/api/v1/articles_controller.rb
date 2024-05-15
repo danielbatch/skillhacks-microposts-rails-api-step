0749047e639cf457f0bfe1639df96cea64e6c23d
danielbatch <104686031+danielbatch@users.noreply.github.com>
2024-05-15 10:23:11 +0000

    投稿作成APIを作ろうまで

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,7 +1,26 @@
+class InvalidRequestError < StandardError; end
+class InvalidStatusError < StandardError; end
+
 class Api::V1::ArticlesController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     status = params[:status]&.to_sym || :published
     articles = Article.where(status: status)
     render json: articles
+  end
+
+  def create
+    raise InvalidRequestError if create_params[:status].nil?
+    raise InvalidStatusError unless Article.statuses.keys.include?(create_params[:status])
+    
+    category = Category.find(create_params[:category_id])
+    article = Article.create!(create_params.merge(category: category))
+    render json: article
+  end
+
+  private
+  def create_params
+    params.require(:article).permit(:title, :body, :status, :category_id)
   end
 end
